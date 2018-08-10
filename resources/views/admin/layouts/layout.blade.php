3834649c183391cc55c4d911cfac89120680272f
Eman Abdelrahman <eman.fcih2015@gmail.com>
2018-08-10 16:21:14 +0200

    edit footer

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -565,40 +565,40 @@
 reserved.
 </footer>
 <!-- jQuery 3 -->
-{!!Html::script('back-end/bower_components/jquery/dist/jquery.min.js')!!}}
+{!!Html::script('back-end/bower_components/jquery/dist/jquery.min.js')!!}
 <!-- jQuery UI 1.11.4 -->
-{!!Html::script('back-end/bower_components/jquery-ui/jquery-ui.min.js')!!}}
+{!!Html::script('back-end/bower_components/jquery-ui/jquery-ui.min.js')!!}
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
 $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-{!!Html::script('back-end/bower_components/bootstrap/dist/js/bootstrap.min.js')!!}}
+{!!Html::script('back-end/bower_components/bootstrap/dist/js/bootstrap.min.js')!!}
 <!-- Morris.js charts -->
-{!!Html::script('back-end/bower_components/raphael/raphael.min.js')!!}}
+{!!Html::script('back-end/bower_components/raphael/raphael.min.js')!!}
 <!-- Sparkline -->
-{!!Html::script('back-end/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')!!}}
+{!!Html::script('back-end/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')!!}
 <!-- jvectormap -->
-{!!Html::script('back-end/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')!!}}
-{!!Html::script('back-end/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')!!}}
+{!!Html::script('back-end/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')!!}
+{!!Html::script('back-end/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')!!}
 <!-- jQuery Knob Chart -->
-{!!Html::script('back-end/bower_components/jquery-knob/dist/jquery.knob.min.js')!!}}
+{!!Html::script('back-end/bower_components/jquery-knob/dist/jquery.knob.min.js')!!}
 <!-- daterangepicker -->
-{!!Html::script('back-end/bower_components/moment/min/moment.min.js')!!}}
-{!!Html::script('back-end/bower_components/bootstrap-daterangepicker/daterangepicker.js')!!}}
+{!!Html::script('back-end/bower_components/moment/min/moment.min.js')!!}
+{!!Html::script('back-end/bower_components/bootstrap-daterangepicker/daterangepicker.js')!!}
 <!-- datepicker -->
-{!!Html::script('back-end/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')!!}}
+{!!Html::script('back-end/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')!!}
 <!-- Bootstrap WYSIHTML5 -->
-{!!Html::script('back-end/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')!!}}
+{!!Html::script('back-end/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')!!}
 <!-- Slimscroll -->
-{!!Html::script('back-end/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')!!}}
+{!!Html::script('back-end/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')!!}
 <!-- FastClick -->
-{!!Html::script('back-end/bower_components/fastclick/lib/fastclick.js')!!}}
+{!!Html::script('back-end/bower_components/fastclick/lib/fastclick.js')!!}
 <!-- AdminLTE App -->
-{!!Html::script('back-end/dist/js/adminlte.min.js')!!}}
+{!!Html::script('back-end/dist/js/adminlte.min.js')!!}
 
 <!-- AdminLTE for demo purposes -->
-{!!Html::script('back-end/dist/js/demo.js')!!}}
+{!!Html::script('back-end/dist/js/demo.js')!!}
 
 @yield('footer')
 
